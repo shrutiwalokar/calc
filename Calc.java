fcbb4ae99088b7bd6aed6c1fb2eaa86b33ac69d4
Shruti <shruti.walokar.it@ghrce.raisoni.net>
2025-12-25 16:12:05 +0530

    Added program for addition

---
+++ Calc.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Calc{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first number: ");
+		int num1 = sc.nextInt();
+
+		System.out.println("Enter second number: ");
+		int num2 = sc.nextInt();
+
+		System.out.println("Addition: " + (num1+num2));
+		sc.close();
+	}
+}
